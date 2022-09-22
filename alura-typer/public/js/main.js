460916ae28a1f11ddba1ba218206751cc2406df4
Allan08 <allan.felizzeti@ksys.com.br>
2022-09-22 17:12:55 -0300

    Funções que auxiliam no estilo e iniciando modolo final

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     atualizaTamanhoFrase()
     inicilizaContadores()
     InicializaCronometro()
+    inicializaMarcadores()
     $("#botao-reiniciar").click(reiniciaJogo)
 })
 
@@ -35,16 +36,50 @@ function InicializaCronometro(){
             console.log(tempoRestante)
             $("#tempo-digitacao").text(tempoRestante)
             if (tempoRestante <= 0){
-                campo.attr("disabled", true)
                 clearInterval(cronometroID)
+                finalizaJogo()
             }
         },1000)
     })
 }
 
+function finalizaJogo(){
+    campo.attr("disabled", true)          
+    campo.toggleClass("campo-desativado")
+    inserePlacar()
+}
+
 // $("#botao-reiniciar").on("click", function(){
 //     console.log('Botão Clicado')
 // })
+
+function inicializaMarcadores(){
+    var frase = $(".frase").text()
+    campo.on("input", function(){
+        var digitado = campo.val()
+        var comparavel = frase.substr(0,digitado.length)
+        if(digitado == comparavel){
+            campo.addClass("borda-verde")
+            campo.removeClass("borda-vermelha")
+        }else {
+            campo.addClass("borda-vermelha")
+            campo.removeClass("borda-verde")
+        }
+    })
+}
+
+function inserePlacar(){
+    var corpotabela = $(".placar").find("tbody")
+    var usuario = "Douglas"
+    var numPalavras = $("#contador-palavras").text()
+
+    var linha = "<tr>"+
+                    "<td>"+ usuario +"</td>"+
+                    "<td>"+ numPalavras +"</td>"+
+                "</tr>";
+
+    corpotabela.append(linha)
+}
 
 function reiniciaJogo(){
     campo.attr("disabled", false)
@@ -53,5 +88,8 @@ function reiniciaJogo(){
     $("#contador-caracteres").text("0")
     $("#tempo-digitacao").text(TempoInicial)
     InicializaCronometro()
+    campo.toggleClass("campo-desativado")
+    campo.removeClass("borda-vermelha")
+    campo.removeClass("borda-verde")
 }
 
